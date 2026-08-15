4cbc895a3a06a238e8e8f5fd1a967906fe431a59
jiangxu <jiangxu01@corp.netease.com>
2026-08-15 17:53:04 +0800

    Switch theme to NexT v7.8.0 (Muse), add TUI article
    - Replace iissnan NexT v5 with theme-next/hexo-theme-next v7.8.0, Muse scheme
    - Disable motion animations and canvas-nest background
    - Replace live2d with floating GIF mascot (bottom-right)
    - Add post: TUI 便捷的终端管理 (Prowl + Finder Quick Action)

--- themes/next/scripts/helpers/font.js
+++ themes/next/scripts/helpers/font.js
@@ -1,25 +1,29 @@
+/* global hexo */
+
 'use strict';
 
-// https://developers.google.com/fonts/docs/getting_started
-module.exports = function() {
-  const config = this.theme.font;
+hexo.extend.helper.register('next_font', () => {
+  const config = hexo.theme.config.font;
 
   if (!config || !config.enable) return '';
 
-  const fontStyles = ':ital,wght@0,300;0,400;0,700;1,300;1,400;1,700';
-  const fontHost = config.host || 'https://fonts.googleapis.com';
+  const fontDisplay = '&display=swap';
+  const fontSubset = '&subset=latin,latin-ext';
+  const fontStyles = ':300,300italic,400,400italic,700,700italic';
+  const fontHost = config.host || '//fonts.googleapis.com';
 
-  // Get a font list from config
-  let fontFamilies = [];
-  ['global', 'title', 'headings', 'posts', 'codes'].forEach(item => {
-    if (config[item]?.family && config[item].external) {
-      fontFamilies = fontFamilies.concat(config[item].family.split(','));
+  //Get a font list from config
+  let fontFamilies = ['global', 'title', 'headings', 'posts', 'codes'].map(item => {
+    if (config[item] && config[item].family && config[item].external) {
+      return config[item].family + fontStyles;
     }
+    return '';
   });
 
-  fontFamilies = fontFamilies.map(name => name.trim().replace(/\s/g, '+') + fontStyles);
-  fontFamilies = [...new Set(fontFamilies)].map(name => 'family=' + name).join('&');
+  fontFamilies = fontFamilies.filter(item => item !== '');
+  fontFamilies = [...new Set(fontFamilies)];
+  fontFamilies = fontFamilies.join('|');
 
   // Merge extra parameters to the final processed font string
-  return fontFamilies ? `<link rel="stylesheet" href="${fontHost}/css2?${fontFamilies}&display=swap&subset=latin,latin-ext">` : '';
-};
+  return fontFamilies ? `<link rel="stylesheet" href="${fontHost}/css?family=${fontFamilies.concat(fontDisplay, fontSubset)}">` : '';
+});
